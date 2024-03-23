0ddbd33cb1dfbd9790703d4aae32d52a13961441
long.nguyen <shenlong1991@gmail.com>
2024-03-23 08:32:51 +0100

    Update handle error

--- common/app_error.go
+++ common/app_error.go
@@ -8,7 +8,7 @@ import (
 
 type AppError struct {
 	StatusCode int    `json:"status_code"`
-	RootErr    error  `json:"_"`
+	RootErr   error  `json:"_"`
 	Message    string `json:"message"`
 	Log        string `json:"log"`
 	Key        string `json:"error_key"`
@@ -44,17 +44,6 @@ func NewUnauthorizedResponse(err error, message, log, key string) *AppError {
 	}
 }
 
-func (e *AppError) RootError() error {
-	if err, ok := e.RootErr.(*AppError); ok {
-		return err.RootError()
-	}
-	return e.RootErr
-}
-
-func (e *AppError) Error() string {
-	return e.RootErr.Error()
-}
-
 func NewCustomError(root error, msg string, key string) *AppError {
 	if root != nil {
 		return NewErrorResponse(root, msg, root.Error(), key)
@@ -66,6 +55,6 @@ func ErrNoPermission(err error) *AppError {
 	return NewCustomError(err, fmt.Sprintf("You have no permission: %s", err.Error()), "NO_PERMISSION_ERROR")
 }
 
-func ErrDB(err error) *AppError {
-	return NewCustomError(err, "Something went wrong with DB", "DB_ERROR")
+func ErrDB(err error) error {
+	return errors.New("something went wrong with Database");
 }
--- modules/items/storage/create_item.go
+++ modules/items/storage/create_item.go
@@ -2,14 +2,12 @@ package storage
 
 import (
 	"context"
-	"fmt"
 	"gogo/common"
 	"gogo/modules/items/model"
 )
 
 func (s *sqlStore) CreateItem(ctx context.Context, data *model.TodoItemsCreation) error {
 	if err := s.db.Create(&data); err.Error != nil {
-		fmt.Print("Error: shenL: ", common.ErrDB(err.Error))
 		return common.ErrDB(err.Error)
 	}
 
